b59d9daa42c83bac7ac6b55d7e84fabd728420e8
kXrock <keremkaya08@hotmail.com>
2024-12-01 18:06:37 +0300

    loginactivity page added

---
+++ app/src/main/java/com/example/novnavex/LogInActivity.kt
@@ -0,0 +1,34 @@
+package com.example.novnavex
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.example.novnavex.databinding.ActivityLoginBinding
+import com.example.novnavex.viewmodel.UserViewModel
+
+class LogInActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var viewModel: UserViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+
+        // ViewModel bağlama
+        viewModel = ViewModelProvider(this).get(UserViewModel::class.java)
+
+        // ViewModel ve LifecycleOwner ayarları
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
+
+        // Login butonuna tıklama olayı
+        binding.logInButton.setOnClickListener {
+            val playerName = binding.textInputLayout.editText?.text.toString()
+            // Burada navigation işlemi yapılabilir veya başka bir işlem gerçekleştirilebilir.
+            // Örneğin:
+            // startActivity(Intent(this, AchievementActivity::class.java))
+        }
+    }
+}
